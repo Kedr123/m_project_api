85a35f3d925039644303666d73e08e0603d28026
Kedr123 <48621337+Kedr123@users.noreply.github.com>
2022-02-02 19:59:54 +0300

    Add JWT token

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('avatar')->default('https://www.visitquilpieshire.com/wp-content/uploads/2017/01/default-avatar-male.jpg');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('birthday');
+            $table->string('type');
             $table->rememberToken();
             $table->timestamps();
         });
